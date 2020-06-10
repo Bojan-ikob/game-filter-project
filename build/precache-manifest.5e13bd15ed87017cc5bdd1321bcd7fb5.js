61e0772c2a1d3849ef4f82e98803433ca1fffebc
Bojan-ikob <66691705+Bojan-ikob@users.noreply.github.com>
2020-06-10 22:30:51 +0200

    run build
    run build

---
+++ build/precache-manifest.5e13bd15ed87017cc5bdd1321bcd7fb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957ed30413b87763cec93be4fa4b7800",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d404afc6bbbe8617631f",
+    "url": "/static/css/main.fcdbe8bf.chunk.css"
+  },
+  {
+    "revision": "7f50b54d0d70b6bc1a20",
+    "url": "/static/js/2.3bf7cbb7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.3bf7cbb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d404afc6bbbe8617631f",
+    "url": "/static/js/main.b32e189e.chunk.js"
+  },
+  {
+    "revision": "7e7cd279c414593561a8",
+    "url": "/static/js/runtime-main.3c72dea9.js"
+  },
+  {
+    "revision": "dcd3727437d20b4817cd5c9c7787717b",
+    "url": "/static/media/logo.dcd37274.png"
+  }
+]);
